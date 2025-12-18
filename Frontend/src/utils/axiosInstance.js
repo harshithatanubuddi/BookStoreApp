8082cc609fb781cc14be44ae4bee7edf248761fa
Harshitha Tanubuddi <harshitha4569@gmail.com>
2025-12-18 08:54:18 +0530

    axiosInstance.js

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4001",
+  baseURL: "https://bookstoreapp-backend-ynkn.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
